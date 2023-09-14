4142546979a6c905f7ec9562cb4a585c57011d7c
Naresh <108452476+nareshmmr@users.noreply.github.com>
2023-09-14 13:03:50 +0400

    Update ConsumerContract.sol

--- consumer-contract/ConsumerContract.sol
+++ consumer-contract/ConsumerContract.sol
@@ -8,26 +8,25 @@ interface IInvokeOracle {
 
 contract ConsumerContract {
     address CONTRACTADDR = 0x902e7B2555A699edE58e6C0A9086701833d996AD;
-    uint256 public requestId;
     address private owner;
-    mapping (uint256 => string) public priceIndexUsedInRequestID;
+    mapping(string => uint256) latestRequestData;
+
 
     constructor() public{
         owner = msg.sender;
     }
 
     //Note, below function will not trigger if you do not put PLI in above contract address
-    function getPriceInfo(string  fromPriceIndex) external returns (uint256) {
+    function getPriceInfo(string  _symbol) external returns (uint256) {
         require(msg.sender==owner,"Only owner can trigger this");
-        (requestId) = IInvokeOracle(CONTRACTADDR).requestData({_authorizedWalletAddress:owner,fromIndex:fromPriceIndex});
-        priceIndexUsedInRequestID[requestId] = fromPriceIndex;
-        return requestId;
+        (latestRequestData[_symbol]) = IInvokeOracle(CONTRACTADDR).requestData({_authorizedWalletAddress:owner,fromIndex:_symbol});
+        return latestRequestData[_symbol];
     }
 
     //TODO - you can customize below function as you want, but below function will give you the pricing value
     //This function will give you last stored value in the contract
-    function show(uint256 _id) external view returns (uint256, uint256) {
-        (uint256 answer, uint256 updatedOn) = IInvokeOracle(CONTRACTADDR).showPrice({_reqid: _id});
-        return (answer,updatedOn);
+    function show(string symbol) external view returns (uint256 _answer, string _symbol, uint256 _timestamp) {
+        (uint256 answer, uint256 updatedOn) = IInvokeOracle(CONTRACTADDR).showPrice({_reqid: latestRequestData[symbol]});
+        return (answer,symbol, updatedOn);
     }
 }
